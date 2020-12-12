37da17567d55e1a73d5a81362ee5e9de08151a45
williamsimms <williamsimms193@gmail.com>
2020-12-12 08:36:20 -0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,72 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.scss'
 
 const App = () => {
+  const fill = useRef<any>()
+
+  const onDragStart = (e: any) => {
+    e.target.className += ' hold'
+    setTimeout(() => {
+      e.target.className = 'invisible'
+    }, 0)
+  }
+
+  const onDragEnd = (e: any) => {
+    e.target.className = 'fill'
+  }
+
+  const onDragOver = (e: any) => {
+    e.preventDefault()
+  }
+
+  const onDragEnter = (e: any) => {
+    e.preventDefault()
+    e.target.className += ' hovered'
+  }
+
+  const onDragLeave = (e: any) => {
+    e.target.className = 'empty'
+  }
+
+  function onDragDrop(e: any) {
+    e.target.className = 'empty'
+    e.target.append(fill.current)
+  }
+
   return (
     <div className='app'>
-      <div className='empty'>
-        <div className='fill' draggable='true'></div>
+      <div
+        className='empty'
+        onDragOver={onDragOver}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDragDrop}>
+        <div className='fill' draggable='true' onDragStart={onDragStart} onDragEnd={onDragEnd} ref={fill}></div>
       </div>
-      <div className='empty'></div>
-      <div className='empty'></div>
-      <div className='empty'></div>
-      <div className='empty'></div>
+      <div
+        className='empty'
+        onDragOver={onDragOver}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDragDrop}></div>
+      <div
+        className='empty'
+        onDragOver={onDragOver}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDragDrop}></div>
+      <div
+        className='empty'
+        onDragOver={onDragOver}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDragDrop}></div>
+      <div
+        className='empty'
+        onDragOver={onDragOver}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDragDrop}></div>
     </div>
   )
 }
